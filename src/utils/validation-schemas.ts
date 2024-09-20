c4b038ea20eb400dd093ac56a177774f92f24911
elforgaani <mohamed.elforgani1999@gmail.com>
2024-09-20 14:39:40 +0200

    Added Some validation schemas!

--- src/utils/validation-schemas.ts
+++ src/utils/validation-schemas.ts
@@ -7,3 +7,16 @@ export const signInValidationSchema = toTypedSchema(
     password: Yup.string().required().min(8).max(16),
   })
 );
+
+export const sendOtpValidationSchema = toTypedSchema(
+  Yup.object({
+    email: Yup.string().required().email().max(30),
+  })
+);
+
+export const signUpValidationSchema = Yup.object().shape({
+  firstName: Yup.string().required().min(2).max(32),
+  lastName: Yup.string().required().min(2).max(32),
+  password: Yup.string().required().min(8).max(32),
+  otp: Yup.string().required().length(6),
+});
