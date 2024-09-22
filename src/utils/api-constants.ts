cd5c1a2f38cf40e623085283db67a1ab2e8a0091
elforgaani <mohamed.elforgani1999@gmail.com>
2024-09-22 23:54:55 +0200

    Added new endpoints!

--- src/utils/api-constants.ts
+++ src/utils/api-constants.ts
@@ -1,7 +1,9 @@
 export const apiConstants = {
-    url: "http://localhost:5002/v1",
-    signIn: "/user/sign-in",
-    sendOtp: "/user/send-otp",
-    signUp: "/user/sign-up",
-  };
-  
+  url: "http://localhost:5002/v1",
+  signIn: "/user/sign-in",
+  sendOtp: "/user/send-otp",
+  signUp: "/user/sign-up",
+  todos: "/todo/get-todos",
+  updateTodo: "/todo/update-todo/",
+  deleteToto: "/delete-todo/",
+};
