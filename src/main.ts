44c56f8b7c2d37d96f544267b497a2aa4529d1bc
elforgaani <mohamed.elforgani1999@gmail.com>
2024-09-23 17:32:44 +0200

    Added Oh vue icons!

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ import App from "./App.vue";
 import { router } from "./router";
 import Toast from "vue-toastification";
 import { createPinia } from "pinia";
+import { addIcons, OhVueIcon } from "oh-vue-icons";
+import { IoTrashOutline, CoPen } from "oh-vue-icons/icons";
 
 const app = createApp(App);
 
 app.use(router);
 app.use(Toast);
 app.use(createPinia());
-
+addIcons(IoTrashOutline, CoPen);
+app.component("v-icon", OhVueIcon);
 app.mount("#app");
