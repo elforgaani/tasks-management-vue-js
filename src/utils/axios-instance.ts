5109f2084e22457ff283abdf8aac197aa63370d6
elforgaani <mohamed.elforgani1999@gmail.com>
2024-09-23 17:03:26 +0200

    Added axios response interceptor!

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,6 +1,42 @@
-import axios from "axios";
+import axios, { type AxiosError, type InternalAxiosRequestConfig } from "axios";
 import { apiConstants } from "./api-constants";
+import { useAuthStore } from "@/stores/auth-store";
 
-export const instance = axios.create({
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean;
+}
+const instance = axios.create({
   baseURL: apiConstants.url,
 });
+
+instance.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as CustomAxiosRequestConfig;
+    const authStore = useAuthStore();
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const response = await axios.get(
+          apiConstants.url + apiConstants.refreshToken,
+          {
+            headers: {
+              Authorization: `Bearer ${authStore.user?.refreshToken}`,
+            },
+          }
+        );
+        console.log(response.data);
+        authStore.setUser(response.data);
+        if (originalRequest) {
+          originalRequest.headers.Authorization = `Bearer ${authStore.user?.accessToken}`;
+        }
+        return instance(originalRequest);
+      } catch (refreshError) {
+        authStore.signOutUser();
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+export { instance };
